5098692e4094e74b6bbcb9058f5f313d2aa85e69
JamesCalingo <james.calingo@gmail.com>
2019-07-29 16:05:34 -0400

    3 files modified

--- src/app/products.ts
+++ src/app/products.ts
@@ -2,17 +2,17 @@ export const products = [
   {
     name: 'Phone XL',
     price: 799,
-    description: 'A large phone with one of the best screens'
+    description: "A large phone with one of the best screens. Just be sure not to destroy it. As long as you're willing to lug this giant thing in your pockets, it's probably our best deal."
   },
   {
     name: 'Phone Mini',
     price: 699,
-    description: 'A great phone with one of the best cameras'
+    description: 'A great phone with one of the best cameras...somehow. Smaller than the Phone Standard but somehow costs MORE because we can do that.'
   },
   {
     name: 'Phone Standard',
     price: 299,
-    description: ''
+    description: 'A standard phone with standard features because it is a standard phone which is BORING! Honestly, why are we offering this because who on earth would buy this?'
   }
 ];
 
